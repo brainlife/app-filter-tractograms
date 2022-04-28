18feeee20c0cd90495c46885e8da36513f87635f
Brad Caron <bacaron245@gmail.com>
2022-04-28 11:40:53 -0400

    Reason: initial commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+track=`jq -r '.track' config.json`
+labels=`jq -r '.labels' config.json`
+
+[ ! -d ./filtered_track ] && mkdir -p filtered_track
+
+[ ! -f ./filtered_track/track.tck ] && connectome2tck ${track} ${labels} ./filtered_ -nodes 1 -exclusive -keep_self -nthreads 8 && mv ./filtered_* ./filtered_track/track.tck
+
+[ ! -f ./filtered_track/track.tck ] && echo "something went wrong. check derivatives" && exit 1 || echo "complete!" && exit 0
