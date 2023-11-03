97583551e067196cc01aaef7ba53cb511cadd029
Brad Caron <bacaron@utexas.edu>
2023-11-03 13:49:15 -0500

    Reason: small fix

--- binarize_labels.py
+++ binarize_labels.py
@@ -5,7 +5,7 @@ def main():
     with open('config.json','r') as config_f:
         config = json.load(config_f)
 
-    labels = config['index']
+    labels = config['labels']
     labels_df = pd.read_csv(labels,header=None)
     labels_df_cleaned = labels_df.copy()
     labels_df_cleaned[0] = [ 1 if f > 0 else 0 for f in labels_df[0] ]
